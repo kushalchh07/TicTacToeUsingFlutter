0883b5e6b9905de9e74b1e6e275953a58b24fc5d
kushalchh07 <kushalchhetri54@gmail.com>
2024-04-30 16:59:24 +0545

    Third Commit

--- lib/goto_screen.dart
+++ lib/goto_screen.dart
@@ -1,4 +1,7 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tic_tac_toe/homepage.dart';
 
@@ -20,33 +23,67 @@ class GoTo extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Text(
-              "TIC TAC TOE",
-              style: newFontOther,
-            ),
-            Image(
-              image: AssetImage("images/tictac.png"),
-              height: size.height * 0.50,
-              width: size.height * 0.50,
-            ),
-            SizedBox(),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => HomePage()));
-              },
-              child: Text(
-                "Play Game",
-                style: TextStyle(color: Colors.black),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 100.0),
+                child: Container(
+                  child: Text(
+                    "TIC TAC TOE",
+                    style: newFontOther,
+                  ),
+                ),
               ),
-              style: ElevatedButton.styleFrom(
-                  fixedSize: Size(450, 60),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12)),
-                  backgroundColor: Colors.white,
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                  textStyle:
-                      TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                child: AvatarGlow(
+                  glowColor: Colors.white,
+                  duration: Duration(seconds: 2),
+                  repeat: true,
+                  startDelay: Duration(seconds: 1),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(style: BorderStyle.none),
+                        shape: BoxShape.circle),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.grey[900],
+                      radius: 80.0,
+                      child: Container(
+                        child: Image(
+                          image: AssetImage("images/tictac.png"),
+                          height: size.height * 0.50,
+                          width: size.height * 0.50,
+                          fit: BoxFit.scaleDown,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomePage()));
+                },
+                child: Text(
+                  "Play Game",
+                  style: TextStyle(color: Colors.black),
+                ),
+                style: ElevatedButton.styleFrom(
+                    fixedSize: Size(450, 60),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)),
+                    backgroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                    textStyle:
+                        TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+              ),
+            ),
+            SizedBox(
+              height: 20,
             ),
           ],
         ),
--- lib/play_with_computer.dart
+++ lib/play_with_computer.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PlayComputer extends StatefulWidget {
   const PlayComputer({super.key});
@@ -8,6 +11,161 @@ class PlayComputer extends StatefulWidget {
 }
 
 class _PlayComputerState extends State<PlayComputer> {
+  List<String> expression = List.filled(9, '');
+  bool _isPlayerTurn = true;
+  // true for player, false for computer
+  final _random = Random();
+  int checkdraw = 0;
+
+  int countComputer = 0;
+  int countPlayer = 0;
+
+  void _ontapped(int index) {
+    // if (expression[index] == '' &&
+    //     !_checkWinner() &&
+    //     !_checkDraw() &&
+    //     _isPlayerTurn) {
+    setState(() {
+      if (_isPlayerTurn) {
+        expression[index] = 'O';
+        checkdraw += 1;
+      }
+      _isPlayerTurn = false;
+
+      {
+        _makeComputerMove();
+        checkdraw += 1;
+      }
+      _isPlayerTurn = !_isPlayerTurn;
+      _checkWinner();
+    });
+    // }
+  }
+
+  void _showDialog(String winner) {
+    if (winner == 'O') {
+      winner = "You";
+    } else {
+      winner = "Computer";
+    }
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text('Game Over'),
+        content: Text('$winner won!'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              _clearboard();
+              Navigator.pop(context);
+            },
+            child: Text('Play Again'),
+          ),
+        ],
+      ),
+    );
+    if (winner == 'O') {
+      countPlayer = countPlayer + 1;
+    } else {
+      countComputer += 1;
+    }
+  }
+
+  void _clearboard() {
+    setState(() {
+      for (int i = 0; i < 9; i++) {
+        expression[i] = '';
+      }
+      checkdraw = 0;
+    });
+  }
+
+  bool _checkWinner() {
+    //   // Implement the logic to check for a winner
+    // Check rows
+    if (expression[0] != '' &&
+        expression[0] == expression[1] &&
+        expression[0] == expression[2]) {
+      _showDialog(expression[0]);
+    }
+    if (expression[3] != '' &&
+        expression[3] == expression[4] &&
+        expression[3] == expression[5]) {
+      _showDialog(expression[3]);
+    }
+    if (expression[6] != '' &&
+        expression[6] == expression[7] &&
+        expression[6] == expression[8]) {
+      _showDialog(expression[6]);
+    }
+    if (expression[0] != '' &&
+        expression[0] == expression[3] &&
+        expression[0] == expression[6]) {
+      _showDialog(expression[0]);
+    }
+    if (expression[1] != '' &&
+        expression[1] == expression[4] &&
+        expression[1] == expression[7]) {
+      _showDialog(expression[1]);
+    }
+    if (expression[2] != '' &&
+        expression[2] == expression[5] &&
+        expression[2] == expression[8]) {
+      _showDialog(expression[2]);
+    }
+    if (expression[0] != '' &&
+        expression[0] == expression[4] &&
+        expression[0] == expression[8]) {
+      _showDialog(expression[0]);
+    }
+    if (expression[2] != '' &&
+        expression[2] == expression[4] &&
+        expression[2] == expression[6]) {
+      _showDialog(expression[2]);
+    } else if (checkdraw == 9) {
+      _showdrawdialogbox();
+    }
+    return false;
+  }
+
+  void _showdrawdialogbox() {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text('Game Over'),
+        content: Text('Game Draw!!'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              _clearboard();
+              Navigator.pop(context);
+            },
+            child: Text('Play Again'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  bool _checkDraw() {
+    //   // Implement the logic to check for a draw
+    if (checkdraw == 9) {}
+    return true;
+  }
+
+  void _makeComputerMove() {
+    // Implement AI algorithm to make optimal move
+    int index = _random.nextInt(9);
+    while (expression[index] != '') {
+      index = _random.nextInt(9);
+    }
+    expression[index] = 'X';
+  }
+
+  static var newFontOther = GoogleFonts.pressStart2p(
+      textStyle: TextStyle(color: Colors.white, letterSpacing: 3));
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -29,6 +187,97 @@ class _PlayComputerState extends State<PlayComputer> {
         height: size.height,
         width: size.width,
         decoration: BoxDecoration(color: bgcolor),
+        child: Column(
+          children: [
+            Expanded(
+                child: Column(
+              children: [
+                Text(
+                  "ScoreBoard",
+                  style: newFontOther,
+                ),
+                SizedBox(
+                  height: 7,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          "You",
+                          style: newFontOther,
+                        ),
+                        Text(
+                          countComputer.toString(),
+                          style: newFontOther,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Computer",
+                          style: newFontOther,
+                        ),
+                        Text(
+                          countPlayer.toString(),
+                          style: newFontOther,
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            )),
+            Expanded(
+              flex: 3,
+              child: GridView.builder(
+                  itemCount: 9,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3),
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                      onTap: () {
+                        _ontapped(index);
+                        print("tapped $index");
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey)),
+                        child: Center(
+                          child: Text(
+                            expression[index],
+                            style: TextStyle(fontSize: 40, color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            Expanded(
+                child: Container(
+                    child: Center(
+                        child: Column(
+              children: [
+                Text(
+                  "TIC TAC TOE",
+                  style: newFontOther,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  "@Created By Kushal",
+                  style: newFontOther,
+                ),
+              ],
+            )))),
+          ],
+        ),
       ),
     );
   }
